c421c32f15c1ca35f9cb8148781ef4f806cf6e28
SymbioticPenguin <cmskinner32@gmail.com>
2019-12-29 15:24:50 -0800

    Final submission of homework solution with txt files included

--- **PyBank**/Solution.py
+++
@@ -1,5 +0,0 @@
-import os
-financials_csv = os.path.join("budget_data.csv")
-print(financials_csv)
-with open(financials_csv,newline = "") as csvfile:
-    reader = csv.reader(csvfile,delimiter = ",")
